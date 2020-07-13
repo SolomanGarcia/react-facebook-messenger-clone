3eba1720af8a7ea410134c63c2d7dc2ccc811201
Soloman Garcia <soloman.garcia@gmail.com>
2020-07-13 11:20:08 -0600

    create input and set state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+
   return (
     <div className="App">
       <h1>Hello</h1>
+
+      <input value={input} onChange={(event) => setInput(event.target.value)} />
+      <button>Send Message</button>
     </div>
   );
 }
