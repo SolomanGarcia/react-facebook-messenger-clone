a92125700737a6e30e259d7a374ac55fba20a1b3
Soloman Garcia <soloman.garcia@gmail.com>
2020-07-13 13:00:20 -0600

    change setMessages to object

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Message from "./Message";
 
 function App() {
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([{}, {}]);
   const [username, setUsername] = useState("");
 
   // useState = variable in REACT
@@ -18,7 +18,7 @@ function App() {
   const sendMessage = (event) => {
     // Logic to send message
     event.preventDefault();
-    setMessages([...messages, input]);
+    setMessages([...messages, { username: username, text: input }]);
     setInput("");
   };
 
@@ -48,7 +48,7 @@ function App() {
 
       {/* displays messages */}
       {messages.map((message) => (
-        <Message text={message} />
+        <Message username={message.username} text={message.text} />
       ))}
     </div>
   );
