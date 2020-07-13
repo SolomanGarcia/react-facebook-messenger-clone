ef88d0cb4ee1a14ab6accc97fbdf37bdd9e3dc42
Soloman Garcia <soloman.garcia@gmail.com>
2020-07-13 13:18:48 -0600

    add styling to message

--- src/Message.js
+++ src/Message.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Card, CardContent, Typography } from "@material-ui/core";
+import "./Message.css";
 
 function Message(props) {
   return (
-    <Card>
+    <Card className="message">
       <CardContent>
         <Typography color="white" variant="h5" component="h2">
           {props.username}: {props.text}
