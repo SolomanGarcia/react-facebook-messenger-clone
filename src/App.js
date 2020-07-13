749c5ea323963ca86b63ad8e383a62f4f3a6623d
Soloman Garcia <soloman.garcia@gmail.com>
2020-07-13 11:29:41 -0600

    create sendMessage function

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import "./App.css";
 
 function App() {
   const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    // Logic to send message
+    setMessages([...messages, input]);
+    setInput("");
+  };
 
   return (
     <div className="App">
       <h1>Hello</h1>
 
       <input value={input} onChange={(event) => setInput(event.target.value)} />
-      <button>Send Message</button>
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   );
 }
