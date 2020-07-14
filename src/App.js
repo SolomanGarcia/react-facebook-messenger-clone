6ed9e399a9fe0c0dd49817f4a920d764b0ecb32c
Soloman Garcia <soloman.garcia@gmail.com>
2020-07-14 15:28:53 -0600

    add more styling

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
         src="https://www.facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100"
         alt="logo"
       />
-      <h1>Hello</h1>
+      <h1>Hello World!</h1>
       <h2>Welcome {username}</h2>
 
       <form className="app__form">
